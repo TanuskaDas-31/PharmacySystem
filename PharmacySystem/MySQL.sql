bb44017ffb6f5f65f862e3d4b93ab4b3b84116e7
TanuskaDas-31 <127971587+TanuskaDas-31@users.noreply.github.com>
2023-10-12 22:15:50 +0530

    Add files via upload

---
+++ PharmacySystem/MySQL.sql
@@ -0,0 +1,8 @@
+create database project;
+use project;
+show tables;
+select * from user_register;
+select * from pharmacist;
+select * from medicine;
+select * from book_medicine;
+select * from allot_stock;
